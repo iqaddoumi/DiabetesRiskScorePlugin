e9f3a07941350cca924ae57f6d1a03473bbab73d
ibqaddoa <ibqaddoa@gmail.com>
2023-05-25 11:27:26 +0200

    Initial commit

---
+++ AdditionPlugin.py
@@ -0,0 +1,20 @@
+from calc_plugin import CalculatorPlugin
+
+
+class AdditionPlugin(CalculatorPlugin):
+    @classmethod
+    def get_name(cls) -> str:
+        return "Calculated Sum"
+
+    @classmethod
+    def required_parameters(cls) -> List[str]:
+        return ["key1", "key2"]
+
+    @classmethod
+    def get_entity_type(cls) -> EntityType:
+        return EntityType.NUMERICAL
+
+    def calculate(self, params: Dict[str, any]) -> any:
+        num1 = params["key1"]
+        num2 = params["key2"]
+        return num1 + num2
---
+++ calc_plugin.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod, ABC
+from typing import Dict, List
+
+
+class CalculatorPlugin(ABC):
+    @classmethod
+    @abstractmethod
+    def get_name(cls) -> str:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def required_parameters(cls) -> List[str]:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_entity_type(cls) -> EntityType:
+        pass
+
+    @abstractmethod
+    def calculate(self, params: Dict[str, any]) -> any:
+        pass
